a8fce0ec41458dd6e01f560d9b7dc773cc906947
kousar2334 <kousar.cse2334@gmail.com>
2026-05-12 23:41:10 +0600

    remvove unwanted code

--- routes/api.php
+++ routes/api.php
@@ -1,46 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ChatController;
-use App\Http\Controllers\Api\UtilityController;
-use App\Http\Controllers\Api\DashboardController;
-use App\Http\Controllers\Api\NotificationController;
 use App\Http\Controllers\Api\ResellerController;
 use App\Http\Middleware\LogApiRequest;
 
-Route::group(['prefix' => 'classified/v1'], function () {
-    Route::get('safety-tips', [UtilityController::class, 'safetyTips']);
-    Route::get('quick-sell-tips', [UtilityController::class, 'quickSellTips']);
-    Route::post('store-image', [UtilityController::class, 'storeImage']);
-
-    Route::group(['middleware' => 'auth:jwt-customer'], function () {
-        Route::get('dashboard-overview', [DashboardController::class, 'customerDashboardOverview']);
-
-        //api/classified/v1/notifications
-        Route::group(['prefix' => 'notifications'], function () {
-            Route::post('list', [NotificationController::class, 'customerAllNotifications']);
-            Route::post('mark-as-read-single-notification', [NotificationController::class, 'markAsRead']);
-            Route::get('mark-as-read-all-notification', [NotificationController::class, 'markAsReadAllNotification']);
-            Route::post('delete-notification', [NotificationController::class, 'deleteNotification']);
-        });
-
-        //api/classified/v1/chat
-        Route::group(['prefix' => 'chat'], function () {
-            Route::post('create-chat', [ChatController::class, 'createChat']);
-            Route::post('list', [ChatController::class, 'chatList']);
-            Route::post('details', [ChatController::class, 'chatDetails']);
-            Route::post('store-new-message', [ChatController::class, 'storeNewMessage']);
-            Route::post('delete', [ChatController::class, 'deleteChat']);
-        });
-    });
-});
 
 // Reseller API (Sanctum token-based)
 Route::prefix('reseller/v1')->middleware(['auth:sanctum', LogApiRequest::class])->group(function () {
     Route::get('/clients',            [ResellerController::class, 'listClients']);
     Route::post('/clients/create',    [ResellerController::class, 'createClient']);
     Route::post('/clients/suspend',   [ResellerController::class, 'suspendClient']);
-    Route::post('/clients/reactivate',[ResellerController::class, 'reactivateClient']);
+    Route::post('/clients/reactivate', [ResellerController::class, 'reactivateClient']);
     Route::get('/credits',            [ResellerController::class, 'creditBalance']);
     Route::get('/plans',              [ResellerController::class, 'availablePlans']);
 });
